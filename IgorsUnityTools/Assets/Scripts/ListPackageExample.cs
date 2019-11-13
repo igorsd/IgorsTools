a47d6ebca3a9326f93d781af8eeb6ee65c68a752
ishtilkind <github@shtilkind.net>
2019-11-12 16:50:59 -0800

    Added Unity Examples

---
+++ IgorsUnityTools/Assets/Scripts/ListPackageExample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.PackageManager.Requests;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+namespace Unity.Editor.Example {
+   static class ListPackageExample
+   {
+       static ListRequest Request;
+    
+       [MenuItem("Window/List Package Example")]
+       static void List()
+       {
+           Request = Client.List();    // List packages installed for the Project
+           EditorApplication.update += Progress;
+       }
+
+       static void Progress()
+       {
+           if (Request.IsCompleted)
+           {
+               if (Request.Status == StatusCode.Success)
+                   foreach (var package in Request.Result)
+                       Debug.Log("Package name: " + package.name);
+               else if (Request.Status >= StatusCode.Failure)
+                   Debug.Log(Request.Error.message);
+
+               EditorApplication.update -= Progress;
+           }
+       }
+   }
+}
